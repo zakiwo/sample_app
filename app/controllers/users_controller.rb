ea30651c764b378d60e0461f5dbb56079ea6d1f3
Zakiwo <hiroki.ozaki39@gmail.com>
2021-07-17 09:03:23 +0000

    [finish]user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_parmas)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def user_parmas
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
